49e6b3ece1411c1b810a16625300afc6bff7e6ec
Nafiz Haque <NAFIZ.HAQUE@uk.aswatson.com>
2025-04-12 13:24:56 +0100

    generic repo

--- MiRS.Gateway/DataAccess/IGenericSQLRepository.cs
+++ MiRS.Gateway/DataAccess/IGenericSQLRepository.cs
@@ -1,6 +1,71 @@
-﻿namespace MiRS.Gateway.DataAccess
+﻿using System.Linq.Expressions;
+
+namespace MiRS.Gateway.DataAccess
 {
-    internal interface IGenericSQLRepository
+    public interface IGenericSQLRepository<TEntity>
+        where TEntity : class
     {
+        /// <summary>
+        /// Gets a single entity based on the provided Id.
+        /// </summary>
+        /// <param name="id">The entity id.</param>
+        /// <param name="cancellationToken">The Cancellation token.</param>
+        /// <returns>Returns a single TEntity object.</returns>
+        Task<TEntity?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Adds an entity to a table.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>A <see cref="TEntity"/> Returns the object that was created.</returns>
+        Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Adds a range of entities to a table.
+        /// </summary>
+        /// <param name="entities">The range of entities to add.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns><see cref="Task"/> representing the asynchronous operation.</returns>
+        Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Updates the entity in table storage with the updated values in the provided entity using the merge UpdateMode.
+        /// </summary>
+        /// <param name="entity">Entity to be updated.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns><see cref="Task"/> representing the asynchronous operation.</returns>
+        Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Deletes an entity from a table.
+        /// </summary>
+        /// <param name="entity">Entity to be delted.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns><see cref="Task"/> representing the asynchronous operation.</returns>
+        Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Deletes a range of entities from a table.
+        /// </summary>
+        /// <param name="entities">The range of entities to delete.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns><see cref="Task"/> representing the asynchronous operation.</returns>
+        Task DeleteManyAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Returns a set of results from a table, based on the provided filter.
+        /// </summary>
+        /// <param name="filter">The filter to run against the table data.</param>
+        /// <param name="continuationToken">The cancellation token.</param>
+        /// <returns><see cref="Task"/> representing the asynchronous operation.</returns>
+        Task<IEnumerable<TEntity>> Query(Expression<Func<TEntity, bool>> filter, string? continuationToken = null);
+
+        /// <summary>
+        /// Gets all entities from a table.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns><see cref="Task"/> representing the asynchronous operation.</returns>
+        Task<IEnumerable<TEntity>> GetAllEntitiesAsync(CancellationToken cancellationToken = default);
     }
 }
--- MiRs.DataAccess/GenericSQLRepository.cs
+++ MiRs.DataAccess/GenericSQLRepository.cs
@@ -1,12 +1,125 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using MiRS.Gateway.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MiRs.DataAccess
 {
-    internal class GenericSQLRepository
+    /// <summary>
+    /// Class that implements IGenericSQLRepository for using Azure SQL database.
+    /// </summary>
+    /// <typeparam name="TEntity">Object for table.</typeparam>
+    public class GenericSQLRepository<TEntity> : IGenericSQLRepository<TEntity>
+        where TEntity : class
     {
+        private readonly RuneHunterDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericSQLRepository(RuneHunterDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        /// <summary>
+        /// Gets a single entity based on the provided Id.
+        /// </summary>
+        /// <param name="id">The entity id.</param>
+        /// <param name="cancellationToken">The Cancellation token.</param>
+        /// <returns>Returns a single TEntity object.</returns>
+        public async Task<TEntity?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _dbSet.FindAsync(id, cancellationToken);
+        }
+
+        /// <summary>
+        /// Adds an entity to a table.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>A <see cref="TEntity"/> Returns the object that was created.</returns>
+        public async Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            await _dbSet.AddAsync(entity, cancellationToken);
+
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        /// <summary>
+        /// Adds a range of entities to a table.
+        /// </summary>
+        /// <param name="entities">The range of entities to add.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns><see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
+        {
+            await _dbSet.AddRangeAsync(entities, cancellationToken);
+
+            await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Updates the entity in the table with the updated values in the provided entity.
+        /// </summary>
+        /// <param name="entity">Entity to be updated.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns><see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            _dbSet.Update(entity);
+
+            await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Deletes an entity from a table.
+        /// </summary>
+        /// <param name="entity">Entity to be delted.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns><see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Deletes a range of entities from a table.
+        /// </summary>
+        /// <param name="entities">The range of entities to delete.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns><see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task DeleteManyAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
+        {
+            _dbSet.RemoveRange(entities);
+            await _context.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Returns a set of results from a table, based on the provided filter.
+        /// </summary>
+        /// <param name="filter">The filter to run against the table data.</param>
+        /// <param name="continuationToken">The cancellation token.</param>
+        /// <returns><see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task<IEnumerable<TEntity>> Query(Expression<Func<TEntity, bool>> filter, string? continuationToken = null)
+        {
+            return await _dbSet.Where(filter).ToListAsync();
+        }
+
+        /// <summary>
+        /// Gets all entities from a table.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns><see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task<IEnumerable<TEntity>> GetAllEntitiesAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbSet.ToListAsync();
+        }
     }
 }
