dce6e4950d0ccf1a913bb4d838f8597e11dd7b9f
fizz_ <fizz_@DESKTOP-0HKGMNR>
2026-02-16 17:59:53 +0000

    added jwt auth

--- src/MiRs.API/Program.cs
+++ src/MiRs.API/Program.cs
@@ -1,18 +1,19 @@
 using Asp.Versioning;
 using Flurl.Http;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using MiRs.DataAccess;
+using MiRs.DiscordClient;
 using MiRs.Domain.Mappers;
+using MiRs.Helpers;
 using MiRs.Interactors;
 using MiRs.RunescapeClient;
-using MiRs.DiscordClient;
 using MiRs.Utils.Helpers;
 using MiRS.Gateway.DataAccess;
 using MiRS.Gateway.DiscordBotClient;
 using MiRS.Gateway.RunescapeClient;
 using System.Reflection;
-using MiRs.Helpers;
 
 namespace MiRs.API
 {
@@ -21,6 +22,13 @@ namespace MiRs.API
         public static void Main(string[] args)
         {
             WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+            IConfigurationSection azureAd = builder.Configuration.GetSection("AzureAd");
+            string azure_tenant_id = azureAd["TenantId"];
+            string azure_client_id = azureAd["ClientId"];
+            string azure_authority = azureAd["Authority"];
+
+
 
             builder.Services.AddDbContext<RuneHunterDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -52,12 +60,44 @@ namespace MiRs.API
                     Version = "v1.0",
                 });
 
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        AuthorizationCode = new OpenApiOAuthFlow
+                        {
+                            AuthorizationUrl = new Uri($"https://login.microsoftonline.com/{azure_tenant_id}/oauth2/v2.0/authorize"),
+                            TokenUrl = new Uri($"https://login.microsoftonline.com/{azure_tenant_id}/oauth2/v2.0/token"),
+                            Scopes = new Dictionary<string, string>
+                            {
+                                { $"api://{azure_client_id}/Admin.Access", "Access API as admin user" }
+                            }
+                        }
+                    }
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme { Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" } },
+                        new[] { $"api://{azure_client_id}/Admin.Access" }
+                    }
+                });
+
                 string xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 
                 options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
                 options.CustomSchemaIds(x => x.FullName);
             });
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = azure_authority;
+                    options.Audience = azure_client_id;
+                });
 
             FlurlHttp.Clients.WithDefaults(builder =>
                 builder.UseSocketsHttpHandler(shh =>
@@ -80,6 +120,8 @@ namespace MiRs.API
 
             builder.Services.AddMediatRContracts();
 
+            builder.Services.AddAuthorization();
+
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("DevCors", policy =>
@@ -100,6 +142,11 @@ namespace MiRs.API
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "MiRS V1");
                 c.RoutePrefix = "swagger";
+
+                // Enable OAuth2 login from Swagger
+                c.OAuthClientId($"{azure_client_id}");
+                c.OAuthAppName("My API Swagger");
+                c.OAuthUsePkce();
             });
 
             app.UseHttpsRedirection();
